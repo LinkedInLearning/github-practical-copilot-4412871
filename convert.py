1b92799f3e9cc7ea8c7a71c9e0facd1b1264db27
Ray Villalobos <ray@planetoftheweb.com>
2023-05-31 22:57:57 -0400

    02_03

--- convert.py
+++ convert.py
@@ -1,12 +1,18 @@
 import os
+import eyed3
 
 # Create a function that reads audio files in the mp3 format from 
-# the 'audio' directory and returns a list of them.
+# the 'audio' directory and returns a list of dictionaries containing
+# the title and comments for each file.
 def get_audio_files():
     audio_files = []
     for file in os.listdir('audio'):
         if file.endswith('.mp3'):
-            audio_files.append(file)
+            audio_file = eyed3.load(os.path.join('audio', file))
+            audio_files.append({
+                'title': audio_file.tag.title,
+                'comments': audio_file.tag.comments
+            })
     return audio_files
 
 print(get_audio_files())
