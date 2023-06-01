d09279a7c7b7a7427bb01362be3187cebb5d963a
Ray Villalobos <ray@planetoftheweb.com>
2023-05-31 20:37:14 -0400

    new recording

--- convert.py
+++ convert.py
@@ -1,19 +1,11 @@
-# read the audio folder and list the files inside it
-
 import os
 
-# path to the audio folder
-path = 'audio'
-
-def create_audio_list_from_files():
-    # list all the files inside the audio folder
-    files = os.listdir(path)
-    # create a list of audio files
+# read mp3 files from the audio directory and list them
+def read_mp3_files():
     audio_files = []
-    # loop through all the files and add the audio files to the list
-    for file in files:
-        if file.endswith('.mp3'):
+    for file in os.listdir("audio"):
+        if file.endswith(".mp3"):
             audio_files.append(file)
     return audio_files
 
-print(create_audio_list_from_files())
+print(read_mp3_files())
